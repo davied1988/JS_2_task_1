a95d1040d4146e385f8715a6741062a61492aa24
Dawid G <dawid.grudziecki@gmail.com>
2017-11-14 18:50:45 +0100

    essential files

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+function getTriangleArea(a, h) {
+	if (a > 0 && h > 0) {
+    return a*h/2;
+	}
+	else {
+	console.log('Nieprawidłowe dane');
+	}
+}
+
+console.log(getTriangleArea(10, 6));
+
+var triangle1Area = getTriangleArea(10, 15);
+console.log(triangle1Area);
+
+var triangle2Area = getTriangleArea(8, 12);
+console.log(triangle2Area);
+
+var triangle3Area = getTriangleArea(5, 13);
+console.log(triangle3Area);
+
