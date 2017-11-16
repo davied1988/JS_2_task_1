7b74912aa17a8637d1f49f0843b3f94a9e2ae191
Dawid G <dawid.grudziecki@gmail.com>
2017-11-17 00:53:33 +0100

    tabs and spaces

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,15 @@
 function getTriangleArea(a, h) {
 	if (a > 0 && h > 0) {
-    return a*h/2;
-	}
-	else {
-	console.log('Nieprawidłowe dane');
+		return a * h / 2;
+		}
+		else {
+		console.log('Nieprawidłowe dane');
 	}
 }
 
 console.log(getTriangleArea(10, 6));
 
-var triangle1Area = getTriangleArea(10, 15);
+var triangle1Area = getTriangleArea(-10, 15);
 console.log(triangle1Area);
 
 var triangle2Area = getTriangleArea(8, 12);
